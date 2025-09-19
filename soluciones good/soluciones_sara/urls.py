169195bd135be00218917a3ef98d13291a423918
sarasoluciones55-droid <giovannygps05@gmail.com>
2025-09-19 07:03:05 -0500

    guardar cambios

--- soluciones good/soluciones_sara/urls.py
+++ soluciones good/soluciones_sara/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mi_app', include ('mi_app.urls', namespace='mi_app'))
+    path('', include ('mi_app.urls', namespace='mi_app'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
